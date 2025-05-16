e5276ee7304ecbe166d085c1d7ad2b0f64677d68
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-16 20:48:23 +0700

    feat(upload): ✨ add FileUpload component for image and video uploads

---
+++ src/components/shared/book/file-upload.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { authenticator } from "@/actions/auth";
+import config from "@/lib/config";
+import { IKImage, ImageKitProvider, IKUpload, IKVideo } from "imagekitio-next";
+import Image from "next/image";
+import { useRef, useState } from "react";
+
+const {
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+interface Props {
+  type: "image" | "video";
+  accept: string;
+  placeholder: string;
+  folder: string;
+  variant: "dark" | "light";
+  onFileChange: (filePath: string) => void;
+  value?: string;
+}
+
+const FileUpload = ({
+  type,
+  accept,
+  placeholder,
+  folder,
+  variant,
+  onFileChange,
+  value,
+}: Props) => {
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string } | null>(null);
+
+  const onError = () => {};
+  const onSuccess = (res: any) => {
+    setFile(res);
+    onFileChange(res.filePath);
+  };
+
+  return (
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        className="hidden"
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+        fileName="test-upload.png"
+      />
+
+      <button
+        className="upload-btn"
+        onClick={(e) => {
+          e.preventDefault();
+          if (ikUploadRef.current) {
+            // @ts-ignore
+            ikUploadRef.current?.click();
+          }
+        }}
+      >
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload-icon"
+          width={20}
+          height={20}
+          className="object-contain"
+        />
+        <p className="text-base text-light-100">Upload a file</p>
+
+        {file && <p className="upload-filename">{file.filePath}</p>}
+      </button>
+
+      {file && (
+        <IKImage
+          alt={file.filePath}
+          path={file.filePath}
+          width={500}
+          height={500}
+        />
+      )}
+    </ImageKitProvider>
+  );
+};
+
+export default FileUpload;
