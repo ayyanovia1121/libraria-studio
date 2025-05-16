d0720847e34fcf4a595496007ff49d0bcb5d15a9
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-16 16:21:38 +0700

    feat(layout): ✨ add Layout component for authentication pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+    return (
+      <main className="auth-container">
+        <section className="auth-form">
+          <div className="auth-box">
+            <div className="flex flex-row gap-3">
+              <Image
+                src="/assets/icons/logo.svg"
+                alt="logo"
+                width={37}
+                height={37}
+              />
+              <h1 className="text-2xl font-semibold text-white">
+                Libraria.studio
+              </h1>
+              <div>{children}</div>
+            </div>
+          </div>
+        </section>
+        <section className="auth-illustration">
+          <Image
+            src="/assets/images/auth-illustration.png"
+            alt="auth illustration"
+            height={1000}
+            width={1000}
+            className="size-full object-cover"
+          />
+        </section>
+      </main>
+    );
+};
+
+export default Layout;
