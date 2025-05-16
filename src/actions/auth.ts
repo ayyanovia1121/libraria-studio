c43c0b5b2e61be92e5034cd4307e51801c5bddad
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-16 20:42:19 +0700

    feat(auth): ✨ implement authenticator function for ImageKit authentication

---
+++ src/actions/auth.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import config from "@/lib/config";
+
+export const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+    }
+
+    const data = await response.json();
+    const {signature, expire, token} = data;
+
+    return {
+      signature,
+      expire,
+      token,
+    };
+
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
