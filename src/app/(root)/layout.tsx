01c244da22fda747871bc57a7900e6b2b22d5655
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-16 15:59:58 +0700

    feat(layout): ✨ replace placeholder with Header component in HomeLayout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,11 @@
+import Header from "@/components/shared/header";
 import React from "react";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        {/* //Todo: Header */}
-        Header
+        <Header />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
