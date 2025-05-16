6d62ebbd09c58e3282b41e82eef1684897760a77
ayyanovia1121 <noviayya1121@gmail.com>
2025-05-16 16:12:25 +0700

    feat(book-list): ✨ add BookList component for displaying a list of popular books

---
+++ src/components/shared/book/book-list.tsx
@@ -0,0 +1,23 @@
+import BookCard from "./book-card";
+
+interface Props {
+  title: string;
+  books: BookProps[];
+  containerClassName?: string;
+}
+
+const BookList = ({ title, books, containerClassName }: Props) => {
+  if (books.length < 2) return;
+  return (
+    <section className={containerClassName}>
+      <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default BookList;
