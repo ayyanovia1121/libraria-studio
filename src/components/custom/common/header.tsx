47e6a77dc7e19dc660c034fc5c7e87cd68c81bfb
ayyanovia1121 <noviayya1121@gmail.com>
2025-10-25 13:28:01 +0700

    feat(auth): add logout button to header

--- src/components/custom/common/header.tsx
+++ src/components/custom/common/header.tsx
@@ -1,11 +1,10 @@
-"use client";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
@@ -13,15 +12,15 @@ const Header = () => {
       </Link>
       <ul className="flex flex-row items-center gap-8">
         <li>
-          <Link
-            href="/library"
-            className={cn(
-              "text-base cursor-pointer capitalize",
-              pathname === "/library" ? "text-light-200" : "text-light-100"
-            )}
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+            className="mb-10"
           >
-            Library
-          </Link>
+            <Button>Logout</Button>
+          </form>
         </li>
       </ul>
     </header>
