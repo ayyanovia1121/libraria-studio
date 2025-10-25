16cbff7110ee21188d48ebff4bfe7f3abb8d13cd
ayyanovia1121 <noviayya1121@gmail.com>
2025-10-25 14:04:54 +0700

    feat(auth): add authentication guards and track user activity

--- src/app/(routes)/(auth)/layout.tsx
+++ src/app/(routes)/(auth)/layout.tsx
@@ -1,36 +1,42 @@
+import { auth } from "@/auth";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
-    return (
-      <main className="auth-container">
-        <section className="auth-form">
-          <div className="auth-box">
-            <div className="flex flex-row gap-3">
-              <Image
-                src="/assets/icons/logo.svg"
-                alt="logo"
-                width={37}
-                height={37}
-              />
-              <h1 className="text-2xl font-semibold text-white">
-                Libraria.studio
-              </h1>
-            </div>
-            <div>{children}</div>
+  const session = await auth();
+
+  if (session) redirect("/");
+
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="logo"
+              width={37}
+              height={37}
+            />
+            <h1 className="text-2xl font-semibold text-white">
+              Libraria.studio
+            </h1>
           </div>
-        </section>
-        <section className="auth-illustration">
-          <Image
-            src="/assets/images/auth-illustration.png"
-            alt="auth illustration"
-            height={1000}
-            width={1000}
-            className="size-full object-cover"
-          />
-        </section>
-      </main>
-    );
+          <div>{children}</div>
+        </div>
+      </section>
+      <section className="auth-illustration">
+        <Image
+          src="/assets/images/auth-illustration.png"
+          alt="auth illustration"
+          height={1000}
+          width={1000}
+          className="size-full object-cover"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default AuthLayout;
--- src/app/(routes)/(root)/layout.tsx
+++ src/app/(routes)/(root)/layout.tsx
@@ -1,10 +1,21 @@
+import { updateUserActivity } from "@/actions/user.action";
+import { auth } from "@/auth";
 import Header from "@/components/custom/common/header";
+import { redirect } from "next/navigation";
+import { after } from "next/server";
 
-const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+const HomeLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+  if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+    await updateUserActivity(session.user.id);
+  });
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        <Header />
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
