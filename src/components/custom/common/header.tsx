486dda4b49042066fae3807b2ff6660c450c5cdc
ayyanovia1121 <noviayya1121@gmail.com>
2025-10-25 14:04:27 +0700

    feat(header): implement user navigation and profile avatar

--- src/components/custom/common/header.tsx
+++ src/components/custom/common/header.tsx
@@ -1,10 +1,14 @@
-import { signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
+"use client";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
+import { getInitials } from "@/utils";
+import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const Header = () => {
+const Header = ({session}: {session: Session}) => {
+  const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
@@ -12,15 +16,24 @@ const Header = () => {
       </Link>
       <ul className="flex flex-row items-center gap-8">
         <li>
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-            className="mb-10"
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
           >
-            <Button>Logout</Button>
-          </form>
+            Library
+          </Link>
+        </li>
+        <li>
+          <Link href="/my-profile">
+            <Avatar>
+              <AvatarFallback className="bg-amber-100">
+               {getInitials(session?.user?.name || "AN")}
+              </AvatarFallback>
+            </Avatar>
+          </Link>
         </li>
       </ul>
     </header>
