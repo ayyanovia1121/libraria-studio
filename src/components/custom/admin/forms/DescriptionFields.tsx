9a3d8e7a3988611ad86fd80f034dd53c20d48dd1
ayyanovia1121 <noviayya1121@gmail.com>
2025-10-29 14:08:47 +0700

    feat(admin-forms): add book description and summary fields

---
+++ src/components/custom/admin/forms/DescriptionFields.tsx
@@ -0,0 +1,61 @@
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { bookSchema } from "@/schemas/validation";
+import { Control } from "react-hook-form";
+import { z } from "zod";
+
+type BookFormValues = z.infer<typeof bookSchema>;
+
+interface FormFieldProps {
+  control: Control<BookFormValues>;
+}
+
+const DescriptionFields = ({ control }: FormFieldProps) => {
+  return (
+    <>
+      <FormField
+        control={control}
+        name="description"
+        render={({ field }) => (
+          <FormItem className="flex flex-col gap-1">
+            <FormLabel className="text-base font-normal text-dark-500">
+              Book Description
+            </FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Book description"
+                {...field}
+                rows={10}
+                className="book-form_input"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={control}
+        name="summary"
+        render={({ field }) => (
+          <FormItem className="flex flex-col gap-1">
+            <FormLabel className="text-base font-normal text-dark-500">
+              Book Summary
+            </FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Book summary"
+                {...field}
+                rows={5}
+                className="book-form_input"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
+
+export default DescriptionFields
